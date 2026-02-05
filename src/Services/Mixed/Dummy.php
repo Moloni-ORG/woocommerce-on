f42fb53fe6764c7130450114b764b73744d10dd8
Fábio Correia <fabio.correia@moloni.com>
2026-02-05 09:55:36 +0000

    Missing ones

---
+++ src/Services/Mixed/Dummy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MoloniOn\Services\Mixed;
+
+class Dummy
+{
+    public function translations()
+    {
+        __('product creation processes.', 'moloni-on');
+        __('product update processes.', 'moloni-on');
+        __('stock update processes.', 'moloni-on');
+        __('Processing product', 'moloni-on');
+        __('Successfully processed', 'moloni-on');
+        __('Error in the process', 'moloni-on');
+        __('Click to see', 'moloni-on');
+        __('Completed', 'moloni-on');
+        __('Starting process...', 'moloni-on');
+        __('No orders selected to process', 'moloni-on');
+        __('Creating document', 'moloni-on');
+        __('Discarding order', 'moloni-on');
+        __('Documents created:', 'moloni-on');
+        __('Documents with errors:', 'moloni-on');
+        __('Orders discarded:', 'moloni-on');
+        __('Orders with errors:', 'moloni-on');
+        __('Close', 'moloni-on');
+        __('Example', 'moloni-on');
+    }
+}
